5a6ce604b9bc02943fe920db2fff3888c9be0b91
websitekid <71786774+websitekid@users.noreply.github.com>
2022-01-10 14:44:43 -0500

    Update ApiPull.js

--- ApiPull.js
+++ ApiPull.js
@@ -1,12 +1,5 @@
-alert("test")
-let requestURL = 'http://api-reborn.orbusvr.com/public/timePlayed/ravenh';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-const CharPull = request.response;
-console.log(request.response);
-alert(request.response);
-}
+fetch(http://api-reborn.orbusvr.com/public/playedTime/ravenh).then(function(response) {
+  response.text().then(function(text) {
+    alert(request.response);
+  });
+});
