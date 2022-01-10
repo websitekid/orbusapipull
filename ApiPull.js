e2538e9eb2cde56a88c05d205486a74ed142bf44
websitekid <71786774+websitekid@users.noreply.github.com>
2022-01-10 14:45:41 -0500

    Update ApiPull.js

--- ApiPull.js
+++ ApiPull.js
@@ -1,4 +1,4 @@
-fetch(http://api-reborn.orbusvr.com/public/playedTime/ravenh).then(function(response) {
+fetch("http://api-reborn.orbusvr.com/public/playedTime/ravenh").then(function(response) {
   response.text().then(function(text) {
     alert(request.response);
   });
